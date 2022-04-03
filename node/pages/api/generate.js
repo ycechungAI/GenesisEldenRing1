80b257c7641267ff4cef6a489ee2eec65289124d
Eugene Chung <ycechung@myseneca.ca>
2022-04-03 17:02:35 -0400

    stash commit 4_3_2022 v1

--- node/pages/api/generate.js
+++ node/pages/api/generate.js
@@ -4,26 +4,44 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
-
+  //training_file: "file-DQ0n1KonqO1BH4gd1wqi7oet",
+  
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req, res) {
   const completion = await openai.createCompletionFromModel({
     model: 'babbage:ft-personal:eugenics-001-2022-04-03-09-54-56',
     prompt: generatePrompt(req.body.word),
-    temperature: 0.21,
+    temperature: 0.13,
     top_p: 1,
-    stop: "\n",
+    stop: ["\n", ".", "->"],
   });
+   //const completion = response; 
   console.log("\n\nTEST1+++++++++++++++\n\n");
-  console.log(completion);
+  console.log(completion.headers);
   console.log("\n\nTEST2+++++++++++++++\n\n");
   console.log(typeof(completion.data.choices));
-  console.log(JSON.stringify(completion.data.choices));
-  res.status(200).json({ result: completion.data.choices[0][0].text });
-}
+  console.log(JSON.stringify(completion.data));
+  res.status(200).json({ result: completion.data.choices[0].text });
+};
 
 function generatePrompt(word) {
   const capitalizedword =
     word[0].toUpperCase() + word.slice(1).toLowerCase();
-  return `\n${word}\n`;
+  console.log(capitalizedword);
+  return `${capitalizedword} ->`;
 }
+
+    /*
+  const response = await openai.createAnswer({
+    search_model: "ada",
+    model: 'babbage:ft-personal:eugenics-001-2022-04-03-09-54-56',  
+    examples_context: "dark souls message translator",
+    documents: ['**** ahead', 'No **** ahead', '**** required ahead', 'Be wary of ****', 'Try ****', 'Likely ****', 'Seek ****', 'Still no ****...',  'Why is it always ****?', 'If only I had a ****...', 'Did not expect ****...', 'Visions of ****...', 'Could this be a ****?', 'Time for ****', '****, O ****', 'Behold, ****!', 'Offer ****', 'Praise the ****!', 'Let there be ****', 'Ahh, ****..'],
+    examples: [["Watch out for the great enemy creature.", "Be wary of strong foe."],["There is an item in front and there is no one ahead", "item ahead and no boss ahead"],
+                ["There is a weapon that deals frozen attack ahead", "armament ahead, frost ahead"]],
+    question: req.body.word,
+    temperature: 0.1,
+    top_p: 1,
+    stop: "\n",
+    });
+  */  
